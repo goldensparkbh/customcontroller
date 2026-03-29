c82a08bba127a5fa4c3d5f8ffdadea9f6e18332d
mjassim2030 <mjassim2030@gmail.com>
2026-03-29 22:39:21 +0300

    'new'

--- dist/assets/OrderSuccess-CZ06xMnu.js
+++ dist/assets/OrderSuccess-CZ06xMnu.js
@@ -1,4 +1,4 @@
-import{r as t,j as n}from"./index-DXoMfwUJ.js";const a=`
+import{r as t,j as n}from"./index-DaUAec20.js";const a=`
 <canvas id="bgCanvas"></canvas>
 <div class="top-nav" style="display:none;"></div>
 <div class="mobile-nav-overlay" id="mobileNavOverlay"></div>
--- dist/assets/PaymentFailed-BLAIAK31.js
+++ dist/assets/PaymentFailed-BLAIAK31.js
@@ -1,4 +1,4 @@
-import{r as t,j as a}from"./index-DXoMfwUJ.js";const n=`
+import{r as t,j as a}from"./index-DaUAec20.js";const n=`
 <canvas id="bgCanvas"></canvas>
 <div class="top-nav" style="display:none;"></div>
 <div class="mobile-nav-overlay" id="mobileNavOverlay"></div>
